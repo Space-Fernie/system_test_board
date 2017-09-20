d1fba7f7666f43bf64d2e7a8e739582399966c84
Space-Fernie <david.fernie@spaceconcordi.ca>
2017-09-19 23:57:21 -0400

    Added getTemp function to get all temperature readings from the compatible sensors. Underlying functions still need to be defined.

--- Software/Arm/Discovery board/test/src/main.cpp
+++ Software/Arm/Discovery board/test/src/main.cpp
@@ -21,12 +21,41 @@
 				  PWR_IN_ADDR[NB_PWR_IN]	= {0x80,0x82},	/* I2C addresses for the INA219 power sensors tied to the input/battery voltage bus */
 				  PWR_3V3_ADDR[NB_PWR_3V3] 	= {0x88,0x8A},	/* I2C addresses for the INA219 power sensors tied to the 3V3 bus */
 				  RTC_ADDR[NB_RTC] 			= {0xD0};		/* I2C address for the DS3232 RTC */
+	
+	/* Misc constants */
+	const uint8_t NB_TOT_THER = NB_THER + NB_BARO + NB_RTC;	/* The barometers and the RTC also have accessible temp sensors */
+
 /* Variables */
+	float therReadings[NB_TOT_THER];
+
 /* Function prototypes */
+	void	getTemp();
+	float	getTherTemp(uint8_t);
+	float	getBaroTemp(uint8_t);
+	float	getRTCTemp(uint8_t);
 
 int main(void){
 	/* Infinite loop */
 	while (1){
 
+	}
+}
+
+void getTemp(){
+	uint8_t i = 0;	/* Shared index for the following loops */
+
+	/* Get temperature from DS1624 */
+	for(i; i < NB_THER; i++){
+		therReadings[i] = getTherTemp(THER_ADDR[i]);
+	}
+
+	/* Get temperature from barometers */
+	for(i; i < NB_THER+NB_BARO; i++){
+		therReadings[i] = getBaroTemp(BARO_ADDR[i-NB_THER]);
+	}
+
+	/* Get temperature from RTC */
+	for(i; i < NB_TOT_THER; i++){
+		therReadings[i] = getRTCTime(RTC_ADDR[i-NB_THER-NB_BARO]);
 	}
 }
